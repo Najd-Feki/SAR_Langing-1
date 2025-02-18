41faa77f7b2423672bfbe2d0e56ffddb010d3d54
hammouda997 <hammouda.msaad@esprit.tn>
2025-02-18 19:30:32 +0100

    add footer logos

--- src/app/home/footer/footer.component.ts
+++ src/app/home/footer/footer.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
+  standalone: true,
   imports: [TranslateModule],
   templateUrl: './footer.component.html',
-  
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+  footerLogo: string = '/assets/icons/footer_en.svg'; 
 
+  constructor(private translate: TranslateService) {
+    this.updateFooterLogo(); 
+    this.translate.onLangChange.subscribe(() => {
+      this.updateFooterLogo();
+    });
+  }
+
+  updateFooterLogo() {
+    const lang = this.translate.currentLang || 'en'; 
+    this.footerLogo = lang === 'ar' ? 'assets/icons/footer_ar.svg' : 'assets/icons/footer_en.svg';
+  }
 }
