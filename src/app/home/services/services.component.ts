b4a4c5c34559bc13fcb1727082b2c76747d20b81
Najd Feki <fekinajd@gmail.com>
2025-02-18 22:49:30 +0100

    services swiper navigations buttons fix

--- src/app/home/services/services.component.ts
+++ src/app/home/services/services.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, ViewChild, ElementRef, Inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  Inject,
+  PLATFORM_ID,
+  AfterViewInit,
+} from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import Swiper from 'swiper';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
@@ -11,13 +18,14 @@ import 'swiper/css/navigation';
   selector: 'app-services',
   templateUrl: './services.component.html',
   standalone: true,
-  imports: [CommonModule, TranslateModule], 
-  styleUrls: ['./services.component.scss']
+  imports: [CommonModule, TranslateModule],
+  styleUrls: ['./services.component.scss'],
 })
 export class ServicesComponent implements AfterViewInit {
-  @ViewChild('carouselContainer', { static: false }) carouselContainer!: ElementRef;
+  @ViewChild('carouselContainer', { static: false })
+  carouselContainer!: ElementRef;
   private swiperInstance!: Swiper;
-  
+
   constructor(
     public translate: TranslateService,
     @Inject(PLATFORM_ID) private platformId: Object
@@ -25,27 +33,81 @@ export class ServicesComponent implements AfterViewInit {
   tagColors: string[] = ['#ECFDF3', '#B2DDFF', '#F9FAFB'];
   Colors: string[] = ['#085D3A', '#1849A9', '#1F2A37'];
   services = [
-    { title: 'SERVICES.CARD_TITLE', content: 'SERVICES.CARD_CONTENT', tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'], primaryAction: 'SERVICES.PRIMARY_ACTION', secondaryAction: 'SERVICES.SECONDARY_ACTION' },
-    { title: 'SERVICES.CARD_TITLE', content: 'SERVICES.CARD_CONTENT', tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'], primaryAction: 'SERVICES.PRIMARY_ACTION', secondaryAction: 'SERVICES.SECONDARY_ACTION' },
-    { title: 'SERVICES.CARD_TITLE', content: 'SERVICES.CARD_CONTENT', tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'], primaryAction: 'SERVICES.PRIMARY_ACTION', secondaryAction: 'SERVICES.SECONDARY_ACTION' },
-    { title: 'SERVICES.CARD_TITLE', content: 'SERVICES.CARD_CONTENT', tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'], primaryAction: 'SERVICES.PRIMARY_ACTION', secondaryAction: 'SERVICES.SECONDARY_ACTION' },
-    { title: 'SERVICES.CARD_TITLE', content: 'SERVICES.CARD_CONTENT', tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'], primaryAction: 'SERVICES.PRIMARY_ACTION', secondaryAction: 'SERVICES.SECONDARY_ACTION' },
-    { title: 'SERVICES.CARD_TITLE', content: 'SERVICES.CARD_CONTENT', tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'], primaryAction: 'SERVICES.PRIMARY_ACTION', secondaryAction: 'SERVICES.SECONDARY_ACTION' },
-    { title: 'SERVICES.CARD_TITLE', content: 'SERVICES.CARD_CONTENT', tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'], primaryAction: 'SERVICES.PRIMARY_ACTION', secondaryAction: 'SERVICES.SECONDARY_ACTION' },
-    { title: 'SERVICES.CARD_TITLE', content: 'SERVICES.CARD_CONTENT', tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'], primaryAction: 'SERVICES.PRIMARY_ACTION', secondaryAction: 'SERVICES.SECONDARY_ACTION' },
-    { title: 'SERVICES.CARD_TITLE', content: 'SERVICES.CARD_CONTENT', tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'], primaryAction: 'SERVICES.PRIMARY_ACTION', secondaryAction: 'SERVICES.SECONDARY_ACTION' }
-
+    {
+      title: 'SERVICES.CARD_TITLE_1',
+      content: 'SERVICES.CARD_CONTENT',
+      tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'],
+      primaryAction: 'SERVICES.PRIMARY_ACTION',
+      secondaryAction: 'SERVICES.SECONDARY_ACTION',
+    },
+    {
+      title: 'SERVICES.CARD_TITLE_2',
+      content: 'SERVICES.CARD_CONTENT',
+      tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'],
+      primaryAction: 'SERVICES.PRIMARY_ACTION',
+      secondaryAction: 'SERVICES.SECONDARY_ACTION',
+    },
+    {
+      title: 'SERVICES.CARD_TITLE_3',
+      content: 'SERVICES.CARD_CONTENT',
+      tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'],
+      primaryAction: 'SERVICES.PRIMARY_ACTION',
+      secondaryAction: 'SERVICES.SECONDARY_ACTION',
+    },
+    {
+      title: 'SERVICES.CARD_TITLE_4',
+      content: 'SERVICES.CARD_CONTENT',
+      tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'],
+      primaryAction: 'SERVICES.PRIMARY_ACTION',
+      secondaryAction: 'SERVICES.SECONDARY_ACTION',
+    },
+    {
+      title: 'SERVICES.CARD_TITLE_5',
+      content: 'SERVICES.CARD_CONTENT',
+      tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'],
+      primaryAction: 'SERVICES.PRIMARY_ACTION',
+      secondaryAction: 'SERVICES.SECONDARY_ACTION',
+    },
+    {
+      title: 'SERVICES.CARD_TITLE_6',
+      content: 'SERVICES.CARD_CONTENT',
+      tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'],
+      primaryAction: 'SERVICES.PRIMARY_ACTION',
+      secondaryAction: 'SERVICES.SECONDARY_ACTION',
+    },
+    {
+      title: 'SERVICES.CARD_TITLE_7',
+      content: 'SERVICES.CARD_CONTENT',
+      tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'],
+      primaryAction: 'SERVICES.PRIMARY_ACTION',
+      secondaryAction: 'SERVICES.SECONDARY_ACTION',
+    },
+    {
+      title: 'SERVICES.CARD_TITLE_8',
+      content: 'SERVICES.CARD_CONTENT',
+      tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'],
+      primaryAction: 'SERVICES.PRIMARY_ACTION',
+      secondaryAction: 'SERVICES.SECONDARY_ACTION',
+    },
+    {
+      title: 'SERVICES.CARD_TITLE_9',
+      content: 'SERVICES.CARD_CONTENT',
+      tags: ['SERVICES.TAG_1', 'SERVICES.TAG_2', 'SERVICES.TAG_3'],
+      primaryAction: 'SERVICES.PRIMARY_ACTION',
+      secondaryAction: 'SERVICES.SECONDARY_ACTION',
+    },
   ];
 
   ngAfterViewInit() {
     if (isPlatformBrowser(this.platformId)) {
       setTimeout(() => {
+        console.log('Platform is browser, initializing Swiper...'); // Debugging log
         const container = this.carouselContainer?.nativeElement;
         if (!container || !container.querySelector('.swiper-slide')) {
           console.warn('Swiper: No slides found, skipping initialization.');
           return;
         }
-  
+
         this.swiperInstance = new Swiper(container, {
           modules: [Pagination, Navigation, Autoplay],
           loop: true,
@@ -53,7 +115,7 @@ export class ServicesComponent implements AfterViewInit {
             delay: 3000,
             disableOnInteraction: false,
           },
-          slidesPerView: 3,
+          slidesPerView: 4,
           spaceBetween: 16,
           navigation: {
             nextEl: '.swiper-button-next',
@@ -69,7 +131,7 @@ export class ServicesComponent implements AfterViewInit {
             },
           },
         });
-  
+        console.log('Swiper initialized!', this.swiperInstance);
         this.updateBulletColors();
       }, 300);
     }
