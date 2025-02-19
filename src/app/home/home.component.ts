f655ba3e44eb74a6f1782ecf925d767901e5f2a7
hammouda997 <hammouda.msaad@esprit.tn>
2025-02-19 01:31:59 +0100

    refactor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,11 +4,12 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { ArticlesNewsComponent } from './articles-news/articles-news.component';
 import { PartnerComponent } from './partner/partner.component';
 import { ServicesTabsComponent } from './services-tabs/services-tabs.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-home',
   standalone: true, 
-  imports: [  HeroComponent,  AboutUsComponent ,ServicesTabsComponent,  PartnerComponent , ArticlesNewsComponent],
+  imports: [  HeroComponent,  AboutUsComponent ,ServicesTabsComponent,  ServicesComponent, PartnerComponent , ArticlesNewsComponent],
     templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
