cf2ca08d7cc26e73e32b0318fd16469593b6be02
hammouda997 <hammouda.msaad@esprit.tn>
2025-02-21 12:18:48 +0100

    add partner translation

--- src/app/home/partner/partner.component.ts
+++ src/app/home/partner/partner.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-partner',
+  standalone: true,
   templateUrl: './partner.component.html',
-  imports:[CommonModule],
-  styleUrls: ['./partner.component.scss']
+  styleUrls: ['./partner.component.scss'],
+  imports: [CommonModule, TranslateModule]
 })
 export class PartnerComponent {
   partners = [
@@ -17,6 +19,19 @@ export class PartnerComponent {
     { logo: 'assets/icons/platformLogo.svg', name: 'Platform Logo' },
     { logo: 'assets/icons/platformLogo.svg', name: 'Platform Logo' }
   ];
+
+  lastModified: string;
+
+  constructor(private translate: TranslateService) {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleDateString('en-GB');
+    const formattedTime = currentDate.toLocaleTimeString('en-GB');
+
+    this.lastModified = this.translate.instant('PARTNER.LAST_MODIFIED', {
+      date: formattedDate,
+      time: formattedTime
+    });
+  }
 
   scrollLeft(container: HTMLElement) {
     container.scrollBy({ left: -200, behavior: 'smooth' });
